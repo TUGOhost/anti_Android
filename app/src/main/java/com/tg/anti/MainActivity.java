9b5b0f97f28f1b49eee8c7869efa64a7576624bf
Your Name <you@example.com>
2022-01-15 07:58:32 -0800

    fix

--- app/src/main/java/com/tg/anti/MainActivity.java
+++ app/src/main/java/com/tg/anti/MainActivity.java
@@ -1,20 +1,8 @@
 package com.tg.anti;
 
 import android.app.Activity;
-import android.content.pm.ApplicationInfo;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-
-import androidx.annotation.RequiresApi;
-
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
-import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class MainActivity extends Activity {
 
@@ -91,39 +79,4 @@ public class MainActivity extends Activity {
     public static native int testIoctl();
 
     //static native String getPropsHash();
-
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    boolean isDualAppEx(String dataDir) {
-        try {
-            String simpleName = "wtf_jack";
-            String testPath = dataDir + File.separator + simpleName;
-            FileOutputStream fos = new FileOutputStream(testPath);
-            FileDescriptor fileDescriptor = fos.getFD();
-            Field fid_descriptor = fileDescriptor.getClass().getDeclaredField("descriptor");
-            fid_descriptor.setAccessible(true);
-            // 获取到 fd
-            int fd = (Integer) fid_descriptor.get(fileDescriptor);
-            // fd 反查真实路径
-            String fdPath = String.format("/proc/self/fd/%d", fd);
-            String realPath = Files.readSymbolicLink(Paths.get(fdPath)).toString();
-            if (!realPath.substring(realPath.lastIndexOf(File.separator))
-                    .equals(File.separator + simpleName)){
-                // 文件名被修改
-                return true;
-            }
-            // 尝试访问真实路径的父目录
-            String fatherDirPath = realPath.replace(simpleName, "..");
-            //Log.d(TAG, "isDualAppEx: " + fatherDirPath);
-            File fatherDir = new File(fatherDirPath);
-            if (fatherDir.canRead()) {
-                // 父目录可访问
-                return true;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return true;
-        }
-        return false;
-    }
 }
